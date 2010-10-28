fb4d7f6b13309614a080b8452b2d175f7d137871
Bill Casarin <jb@jb55.com>
2010-10-28 02:41:19 +0000

    Initial commit

---
+++ test.js
@@ -0,0 +1,22 @@
+
+require.paths.push(__dirname + '/lib');
+var storage = require('storage');
+
+var testFile = "data/song.mp3"
+
+storage.save(testFile, function(s3) {
+
+  s3.on("error", function (err) {
+    console.log("Error:", err);
+  });
+
+  s3.on("progress", function (percentSent) {
+    if (+percentSent % 10 == 0)
+      console.log("Percent: " + percentSent + "%");
+  });
+
+  s3.on("complete", function (hadError) {
+      console.log("Transfer of " + testFile + " completed");
+  });
+
+})
