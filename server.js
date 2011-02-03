4ac339b1081bcfba001e34d0132536bd83a6b16b
Bill Casarin <jb@jb55.com>
2011-02-03 16:38:23 +0000

    Added basic database support

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-require.paths.push(__dirname + '/lib');
 
 var express = require('express')
   , _ = require('underscore')._
+  , db = require('./lib/db')
+  , User = require('./lib/user')
+  , storage = require('./lib/storage')
   , formidable = require('formidable')
-  , storage = require('storage')
   , fs = require('fs')
   , util = require('util')
   ;
 
+// Global (for testing only)
+var GLOBALS = {};
+GLOBALS.songData = [];
+
 // Create the express server
 var app = express.createServer();
+db.cs = db.buildConnectionString(
+  "localhost", 5432, "postgres", "postgres", "cloudvibe");
 
 // Configuration {{{
 // Shared configuration
@@ -38,40 +45,83 @@ app.configure('developement', function () {
 //  });
 
 function viewData (d) {
+  d.artists = selectUniq(GLOBALS.songData, function (s) { return s.artist; });
+  d.titles = selectUniq(GLOBALS.songData, function (s) { return s.title; });
   return { locals: d };
 }
+
+function selectUniq(l, fn) {
+  return _(l)
+    .chain()
+    .map(fn)
+    .uniq()
+    .value();
+}
+
 // Misc }}}
 
+//===----------------------------------------------------------------------===//
 // Root controller
+//===----------------------------------------------------------------------===//
 app.get('/', function (req, res) {
-  console.log(req);
-  res.render("default");
+  res.render("default", viewData({}));
 });
 
 // User controller {{{
+
+//===----------------------------------------------------------------------===//
+// [GET] Root user controller
+//   /user/bill
+//===----------------------------------------------------------------------===//
 app.get('/user/:user', function (req, res) {
   var user = req.params.user;
-  res.render('user', viewData({user: user}));
+  User.exists(db.cs, user, function(err, userExists){
+    if (!userExists) {
+      res.writeHead(200);
+      res.write("User doesn't exist");
+      res.end();
+      return;
+    }
+
+    res.render('user', viewData({user: user}));
+  });
 });
 
+
+//===----------------------------------------------------------------------===//
+// [PUT] Put song controller
+//   /user/bill
+//===----------------------------------------------------------------------===//
 app.put('/user/:user', function (req, res) {
-  var user = req.params.user;
-  var file = '/tmp/' + user + '-test';
+  var urlUser = req.params.user;
+  User.exists(db.cs, urlUser, function(err, userExists){
+    if (!userExists) {
+      res.writeHead(400);
+      res.write("User doesn't exist");
+      res.end();
+    }
 
-  var ws = fs.createWriteStream(file);
+    var file = '/tmp/' + user + '-test';
+    var ws = fs.createWriteStream(file);
 
-  req.addListener('data', function (d) {
-    ws.write(d);
+    req.addListener('data', function (d) {
+      ws.write(d);
+    });
+
+    req.addListener('end', function () {
+      ws.end();
+      res.writeHead(201);
+      res.end();
+    });
+
   });
-
-  req.addListener('end', function () {
-    ws.end();
-    res.writeHead(201);
-    res.end();
-  });
-
 });
 
+
+//===----------------------------------------------------------------------===//
+// [POST] Song upload controller
+//   /user/bill/upload
+//===----------------------------------------------------------------------===//
 app.post('/user/:user/upload', function (req, res) {
   var form = new formidable.IncomingForm();
   var user = req.params.user;
@@ -100,6 +150,20 @@ app.post('/user/:user/upload', function (req, res) {
     res.render("upload_complete", viewData({ fileName: fileName }));
   });
 });
+
+
+//===----------------------------------------------------------------------===//
+// [POST] Sync web method
+//   /user/bill/sync
+//===----------------------------------------------------------------------===//
+app.post('/user/:user/sync', function (req, res) {
+  req.addListener('data', function (d) {
+    GLOBALS.songData = JSON.parse(d.toString());
+  });
+
+  res.send("ok");
+});
+
 // User controller }}}
 
 // Start listening for requests
--- test.js
+++ test.js
@@ -1,11 +1,10 @@
-
+/*
 require.paths.push(__dirname + '/lib');
 var storage = require('storage');
 
 var testFile = "data/song.mp3"
 
 storage.save(testFile, function(s3) {
-
   s3.on("error", function (err) {
     console.log(err);
   });
@@ -18,5 +17,27 @@ storage.save(testFile, function(s3) {
   s3.on("complete", function (hadError) {
       console.log("Transfer of " + testFile + " completed");
   });
-
 })
+*/
+
+var User = require("./lib/user");
+var db = require("./lib/db");
+var cs = db.buildConnectionString(
+  "localhost", 5432, "postgres", "postgres", "cloudvibe");
+
+var testUser = "bill";
+
+db.log = console.log;
+
+User.exists(cs, testUser, function(err, userExists){
+  if (err) throw err;
+  if (!userExists) {
+    User.register(cs, testUser, "derp", function(err, user){
+      if (err) throw err;
+      console.log("Created user", user);
+    });
+  } else {
+    console.log("User", testUser, "exists");
+  }
+});
+
